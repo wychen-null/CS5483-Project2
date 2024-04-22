6dc9a761ffdb9a545f5764a069060eea67da0907
wychen_huawei <wychen0524@aliyun.com>
2024-04-22 11:30:13 +0800

    submit final data

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import random
-
+import pickle
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import *
@@ -12,20 +12,14 @@ random.seed(4487)
 
 # load data from file
 def load_data():
-    xl = pd.ExcelFile('C:/Users/lijia/PycharmProjects/CancerDetection/data/data2.xlsx')
-    # print(xl.sheet_names)
+    xl = pd.ExcelFile('C:/Users/HUAWEI/Desktop/CancerDetection/preprocess/data2.xlsx')
     dfs = {sheet: xl.parse(sheet) for sheet in xl.sheet_names}
 
     data1 = dfs['7']
     data2 = dfs['1'].loc[:, ['Patient', 'Age at Diagnosis']].drop([554]).drop_duplicates()
-    data3 = pd.read_csv('C:/Users/lijia/PycharmProjects/CancerDetection/data/data1.csv')
-    # Median cfDNA Fragment Size (bp), GC Corrected Fragment Ratio Profile, age
-    # PA Score,Patient Type,% of Mapped Reads Mapping to Mitochondria, 39 z score
+    data3 = pd.read_csv('C:/Users/HUAWEI/Desktop/CancerDetection/preprocess/data1.csv')
     combined_data = data1.set_index('Patient').join(data2.set_index('Patient')).join(data3.set_index('Patient'))
-
-    combined_data = combined_data[~combined_data['Patient Type'].isin(['Duodenal Cancer'])]  # 移除十二指肠癌
-    le = preprocessing.LabelEncoder().fit(combined_data['Patient Type'])
-    combined_data['label'] = le.transform(combined_data['Patient Type'])  # transform to label
+    combined_data['label'] = np.where(combined_data['Patient Type'] == 'Healthy', 0, 1)  # transform to label
     combined_data = combined_data.drop(['Patient Type'], axis=1)
     combined_data.to_csv('data.csv', index=False)
     print(
@@ -35,9 +29,7 @@ def load_data():
     x = combined_data.iloc[:, 0:44]
     x[isnan(x)] = 0
     y = combined_data.iloc[:, 44]
-    label = []
-    for i in range(0, 8):
-        label.append(le.inverse_transform([i])[0])
+    label=np.where(combined_data['label'] == 0, 'Healthy', 'Postive')
 
     print(type_of_target(x))
     print(type_of_target(y))
@@ -55,12 +47,6 @@ def load_data():
 def train_test_split(x, y):
     trainX, testX, trainY, testY = \
         model_selection.train_test_split(x, y, train_size=0.7, test_size=0.3, random_state=4487)
-
-    # scaler = preprocessing.StandardScaler()  # make scaling object
-    # trainXn = scaler.fit_transform(trainX)  # use training data to fit scaling parameters
-    # testXn = scaler.transform(testX)  # apply scaling to test data
-    #
-    # return trainXn, trainY, testXn, testY
     return trainX, trainY, testX, testY
 
 
@@ -71,10 +57,13 @@ def plot_roc(validY, validProb):
 
     print("ROC_AUC :", roc_auc)
 
-    plt.plot(fpr, tpr, 'k-', lw=2)
+    fig = plt.figure()
+    fig.patch.set_facecolor('#e8e8f8')
+    plt.plot(fpr, tpr, lw=2, color='#7777cb')
     plt.title('AUC={:.4f}'.format(roc_auc))
     plt.xlabel('FPR')
     plt.ylabel('TPR')
+    plt.gca().set_facecolor('#e8e8f8')
 
     plt.grid(True)
     plt.show()
@@ -82,4 +71,14 @@ def plot_roc(validY, validProb):
 
 if __name__ == '__main__':
     x, y, label = load_data()
+    data = {
+    'x': x,
+    'y': y,
+    'label': label
+}
+
+# 打开一个文件以便写入
+    with open('data.pkl', 'wb') as file:
+    # 使用pickle的dump方法将数据序列化至文件
+        pickle.dump(data, file)
 
